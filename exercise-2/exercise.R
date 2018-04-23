a85c7ee3a3984ff487fb4326cdf26c31638ddfaa
Sarah Liu <swliu79@uw.edu>
2018-04-23 16:21:54 -0700

    Went through in class!

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,41 +3,53 @@
 # load relevant libraries
 library(httr)
 library(jsonlite)
+library(dplyr)
 
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
+source('apikey.R')
 
+#It'll just run the line of code
 
 # Create a variable `movie.name` that is the name of a movie of your choice.
-
+movie_name <- "Coco"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
 # The resource is `reviews/search.json`
 # See the interactive console for parameter details:
 #   https://developer.nytimes.com/movie_reviews_v2.json
-#
-# You should use YOUR api key (as the `api-key` parameter)
-# and your `movie.name` variable as the search query!
+base_uri <- GET("https://api.nytimes.com/svc/movies/v2/")
+resource <- GET("reviews/search.json")
+query_params <- list("api-key" = nty_apikey, query = movie_name)
 
+# You should use YOUR api key (as the `api-key` parameter)
+# and your `movie.name` variable as the search query! CHECK!
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
-
+response <- GET(paste0(base_uri, resource), query_params)
+body <- fromJSON(content(response, "text"))
 
 # What kind of data structure did this produce? A data frame? A list?
-
+is.data.frame(body)
+is.lis(body)
 
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
 # Use functions such as `names()`, `str()`, etc.
-
+names(body)
+#once found the data area
+names(body$results)
 
 # Flatten the movie reviews content into a data structure called `reviews`
-
+reviews <- flatten(body$results)
 
 # From the most recent review, store the headline, short summary, and link to
 # the full article, each in their own variables
+first_review <- reviews[1, ]
+headline <- first_review$headline
+summary <- first_review$summary
 
 
 # Create a list of the three pieces of information from above. 
